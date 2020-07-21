60bd69b22d608817f02016574a6e9b02b908a34f
Andrew A. Stenipanin <and5251@yandex.ru>
2020-07-21 19:18:48 +0300

    User registration

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is the scream body",
+      createdAt: "2020-07-21T00:04:05.456Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
