96093132ade5f5fc6692034403d26b5bf25ccf10
Andrew A. Stenipanin <and5251@yandex.ru>
2020-07-22 01:50:29 +0300

    Refactoring

---
+++ functions/utils/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyA_F9_DcIF4RK9errfU5jH55JGlVIJ8sG8",
+  authDomain: "socialape-23b23.firebaseapp.com",
+  databaseURL: "https://socialape-23b23.firebaseio.com",
+  projectId: "socialape-23b23",
+  storageBucket: "socialape-23b23.appspot.com",
+  messagingSenderId: "657736486764",
+  appId: "1:657736486764:web:dcf2606b6ab338bf4d2d49",
+  measurementId: "G-J5X0N0BEVH",
+};
