6f406fdc1db93f97b63a632e7da72f6f0578a42c
Andrew A. Stenipanin <and5251@yandex.ru>
2020-07-23 20:18:23 +0300

    like, unlike and delete scream

--- dbschema.js
+++ dbschema.js
@@ -1,11 +1,54 @@
 let db = {
-  screams: [
+  users: [
     {
-      userHandle: "user",
-      body: "this is the scream body",
-      createdAt: "2020-07-21T00:04:05.456Z",
-      likeCount: 5,
-      commentCount: 2,
+      userId: 'OaPkK7HVMFNjWOpzdoopTVcPbhj1',
+      email: 'user@gmail.com',
+      handle: 'user',
+      imageUrl: 'image/dsadasdas/asdfafds',
+      bio: 'Hello, my name is user, nice to mett you',
+      website: 'https://user.com',
+      location: 'London, UK'
     },
   ],
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'This is a sample scream',
+      createdAt: '2019-03-15T10:59:52.798Z',
+      likeCount: 5,
+      commentCount: 3
+    }
+  ],
+  comments: [
+    {
+      userHandle: 'user',
+      screamId: 'dsajkjafnmndsfsdnfsdjknf',
+      body: 'nice one mate!',
+      createdAt: '2020-06-15T10:59:52.798Z'
+    }
+  ]
+};
+
+const userDetails = {
+  // Redux data
+  credentials: {
+    userId: 'N43KJ5H43KJHREW4J5H3JWMERHB',
+    email: 'user@email.com',
+    handle: 'user',
+    createdAt: '2019-03-15T10:59:52.798Z',
+    imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+    bio: 'Hello, my name is user, nice to meet you',
+    website: 'https://user.com',
+    location: 'Lonodn, UK'
+  },
+  likes: [
+    {
+      userHandle: 'user',
+      screamId: 'hh7O5oWfWucVzGbHH2pa'
+    },
+    {
+      userHandle: 'user',
+      screamId: '3IOnFoQexRcofs5OhBXO'
+    }
+  ]
 };
--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,15 @@ const app = require("express")();
 
 const FBAuth = require("./utils/fbAuth");
 
-const { getAllScreams, postOneScream, getScream, commentOnScream } = require("./handlers/screams");
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream,
+  likeScream,
+  unlikeScream,
+  deleteScream
+} = require("./handlers/screams");
 const {
   signup,
   login,
@@ -16,11 +24,11 @@ const {
 // Scream routes
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
-app.get('/scream/:screamId', getScream)
-//TODO: delete scream
-// TODO: like a scream
-// TODO: unlike a scream
-app.post('/scream/:screamId/comment', FBAuth, commentOnScream)
+app.get("/scream/:screamId", getScream);
+app.delete('/scream/:screamId', FBAuth, deleteScream)
+app.get("/scream/:screamId/like", FBAuth, likeScream);
+app.get("/scream/:screamId/unlike", FBAuth, unlikeScream);
+app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
 
 // users routes
 app.post("/signup", signup);
